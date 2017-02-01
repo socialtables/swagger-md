3736a61731b3265012bc8468590b64a5cfea736d
Daniel Dombrowsky <ddombrow@gmail.com>
2017-02-01 17:02:25 -0500

    adding version and correcting schemes

--- src/generators/intro-generator.js
+++ src/generators/intro-generator.js
@@ -3,7 +3,8 @@ const api = {
   generateApiIntro(swagger_spec) {
     const api_info = swagger_spec.info;
     const header = `# ${api_info.title}, version ${api_info.version}`;
-    const base_url = `Base URL: ${swagger_spec.schemes.join('|') || "http|https"}://${swagger_spec.host}${swagger_spec.basePath || ''}`;
+    const schemes = swagger_spec.schemes ? swagger_spec.schemes.join('|') : "http|https";
+    const base_url = `Base URL: ${schemes}://${swagger_spec.host}${swagger_spec.basePath || ''}`;
     return `${header}\n\n${base_url}`;
   },
 
